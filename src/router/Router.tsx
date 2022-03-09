54ee824272e574012a78608062964cd55c9168f4
Ronald Prato <ronald.prato@unet.edu.ve>
2022-03-09 00:32:00 -0500

    withCredentials removed

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -23,9 +23,7 @@ const InstantRedirect = ({
 };
 
 const HANKU_SERVER_URL = process.env.REACT_APP_HANKU_SERVER_URL || "";
-const socket: Socket = io(HANKU_SERVER_URL, {
-  withCredentials: true,
-});
+const socket: Socket = io(HANKU_SERVER_URL);
 
 export const Router = () => {
   return (
