519b7d74859bbc3f7b6fea83cdb0abed7e53451b
Ronald Prato <ronald.prato@unet.edu.ve>
2022-03-09 22:05:46 -0500

    back arrow to exit queue logic restored

--- src/modules/Queue/pages/QueueScreen/QueueScreen.tsx
+++ src/modules/Queue/pages/QueueScreen/QueueScreen.tsx
@@ -65,9 +65,9 @@ export const QueueScreen: FC<{ socket: Socket }> = ({ socket }) => {
 
   const handleGoBack = () => {
     // TODO: Add logic to get off of the queue if youre in
-    // socket.emit("exit-queue", user.uid, () => {});
-    // navigate("/home", { replace: true });
-    socket.emit("check-queue");
+    socket.emit("exit-queue", user.uid, () => {});
+    navigate("/home", { replace: true });
+    // socket.emit("check-queue");
   };
 
   const handleExitQueue = () => {
