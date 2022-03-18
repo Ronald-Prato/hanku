7071eb47d2f1bf5cebac383184cc741c13a3291d
Ronald Prato <ronald.prato@unet.edu.ve>
2022-03-18 15:47:24 -0500

    ngrok changed

--- src/gun.ts
+++ src/gun.ts
@@ -4,7 +4,7 @@ import "gun/axe";
 
 // const relayPeer = process.env.RELAY_SERVER_URL
 // const gun = Gun(["http://3.236.91.71:8765/gun"]);
-const gun = Gun(["https://e867-190-27-135-133.ngrok.io/gun"]);
+const gun = Gun(["https://b146-190-27-135-133.ngrok.io/gun"]);
 const user = gun.user().recall({ sessionStorage: true });
 
 export { gun, user };
